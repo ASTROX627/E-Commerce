71c77097411bbb8ce1db3687252fcc3bc3d0a754
astrox627 <ar.noornahal42@gmail.com>
2026-08-10 13:17:08 +0330

    use argon2 insted of bcryptjs

--- backend/src/utils/password.ts
+++ backend/src/utils/password.ts
@@ -1,13 +1,19 @@
-import bcrypt from "bcryptjs";
+import argon2 from "argon2";
+
+const ARGON2_OPTIONS: argon2.HashOptions = {
+  type: argon2.argon2id,
+  memoryCost: 19456,
+  timeCost: 2,
+  parallelism: 1,
+};
 
 export async function hashPassword(password: string): Promise<string> {
-  const salt = await bcrypt.genSalt(10);
-  return bcrypt.hash(password, salt);
+  return argon2.hash(password, ARGON2_OPTIONS);
 }
 
 export async function comparePassword(
   password: string,
   hashedPassword: string,
 ): Promise<boolean> {
-  return bcrypt.compare(password, hashedPassword);
+  return argon2.verify(hashedPassword, password);
 }
