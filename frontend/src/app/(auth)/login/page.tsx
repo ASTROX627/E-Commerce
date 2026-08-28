703b15a7661c4add9c3abc40102894f12c31072e
astrox627 <ar.noornahal42@gmail.com>
2026-08-28 22:16:08 +0330

    initialize frontend

---
+++ frontend/src/app/(auth)/login/page.tsx
@@ -0,0 +1,5 @@
+export default async function Login() {
+  return(
+    <h1>Login page</h1>
+  )
+}
---
+++ frontend/src/app/(auth)/signup/page.tsx
@@ -0,0 +1,6 @@
+export default async function Signup() {
+  return(
+    <h1>Signup page</h1>
+  )
+}
+
