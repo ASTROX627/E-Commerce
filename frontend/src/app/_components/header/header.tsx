729bd038b9990b191c364f33fb38530e92ea54ef
astrox627 <ar.noornahal42@gmail.com>
2026-08-30 18:28:27 +0330

    complete header for xl devices

--- frontend/src/app/_components/header/header.tsx
+++ frontend/src/app/_components/header/header.tsx
@@ -16,26 +16,28 @@ export const Header: React.FC = () => {
   return (
     <>
       <div className="mt-8 container mx-auto">
-        <div className="lg:flex justify-between">
-          <div className="flex justify-between items-center">
-            <IconMenu width={21} height={14} className="lg:hidden" />
-            <div className="flex gap-x-3 items-center">
-              <Image
-                src="/Logo.png"
-                width={40}
-                height={27}
-                alt="logo"
-                className="lg:w-12.25 lg:h-8.25"
-              />
-              <h1 className="font-bold text-xl lg:text-3xl">Luminae</h1>
+        <div className="xl:flex justify-between items-center">
+          <div className="lg:flex justify-between items-center gap-x-5">
+            <div className="flex justify-between items-center">
+              <IconMenu width={21} height={14} className="lg:hidden" />
+              <div className="flex gap-x-3 items-center">
+                <Image
+                  src="/Logo.png"
+                  width={40}
+                  height={27}
+                  alt="logo"
+                  className="lg:w-12.25 lg:h-8.25"
+                />
+                <h1 className="font-bold text-xl lg:text-3xl">Luminae</h1>
+              </div>
+              <IconCard width={24} height={24} className="lg:hidden" />
             </div>
-            <IconCard width={24} height={24} className="lg:hidden" />
+            <SearchBox />
           </div>
-          <SearchBox />
+          <TopNavigation />
         </div>
-        <TopNavigation />
       </div>
-      <BottomHeader/>
+      <BottomHeader />
     </>
   );
 };
--- frontend/src/app/_components/header/top-navigation.tsx
+++ frontend/src/app/_components/header/top-navigation.tsx
@@ -5,7 +5,7 @@ import { NavigationSocialItems } from "./navigation-social-items";
 
 export const TopNavigation: React.FC = () => {
   return (
-    <div className="hidden lg:flex items-center justify-between mt-5">
+    <div className="hidden lg:flex items-center justify-between mt-5 xl:gap-x-7">
       <NavigationMenuItems />
       <NavigationSocialItems />
     </div>
